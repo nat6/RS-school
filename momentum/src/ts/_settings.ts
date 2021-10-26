5fe36bf63f478080a30143f7687013396b5dbf66
nat6 <nat6dv@gmail.com>
2021-10-26 13:11:15 +0300

    feat: add second language

--- momentum/src/ts/_settings.ts
+++ momentum/src/ts/_settings.ts
@@ -1,5 +1,18 @@
+import returnLang from "./_language";
+
 const settings = (<HTMLElement> document.querySelector('.settings'));
 const settingsButton = document.querySelector('.settings-button');
+
+const settingsPlayerBox = (<HTMLElement> document.querySelector('#settings-player'));
+const settingsWeatherBox = (<HTMLElement> document.querySelector('#settings-weather'));
+const settingsTimeBox = (<HTMLElement> document.querySelector('#settings-time'));
+const settingsDateBox = (<HTMLElement> document.querySelector('#settings-date'));
+const settingsGreetingBox = (<HTMLElement> document.querySelector('#settings-greeting'));
+const settingsQuoteBox = (<HTMLElement> document.querySelector('#settings-quote'));
+const settingsTodoBox = (<HTMLElement> document.querySelector('#settings-todo'));
+const settingsControlsBox = (<HTMLElement> document.querySelector('#settings-controls'));
+const settingsImgBox = (<HTMLElement> document.querySelector('#settings-img'));
+
 
 let displaySettings = false;
 
@@ -20,3 +33,32 @@ function showSettings() {
 settingsButton?.addEventListener('click', showSettings);
 
 
+export default function changeSettingLang() {
+  let lang = returnLang();
+
+  if (lang == 'ru') {
+    settings.style.width = '400px';
+    settingsImgBox.textContent = 'Источник изображений';
+    settingsPlayerBox.textContent = 'Плеер';
+    settingsWeatherBox.textContent = 'Погода';
+    settingsTimeBox.textContent = 'Время';
+    settingsDateBox.textContent = 'Дата';
+    settingsGreetingBox.textContent = 'Приветствие';
+    settingsQuoteBox.textContent = 'Цитата';
+    settingsTodoBox.textContent = 'Список дел';
+    settingsControlsBox.textContent = 'Управление';
+  }
+
+  else {
+    settings.style.width = '360px';
+    settingsImgBox.textContent = 'Source of images';
+    settingsPlayerBox.textContent = 'Player';
+    settingsWeatherBox.textContent = 'Weather';
+    settingsTimeBox.textContent = 'Time';
+    settingsDateBox.textContent = 'Date';
+    settingsGreetingBox.textContent = 'Greeting';
+    settingsQuoteBox.textContent = 'Quote';
+    settingsTodoBox.textContent = 'ToDo';
+    settingsControlsBox.textContent = 'Controls';
+  }
+}
