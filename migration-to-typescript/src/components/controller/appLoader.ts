7f1304866a3250e3c4b93a6107c5a733fe438742
nat6 <nat6dv@gmail.com>
2021-12-06 22:13:50 +0300

    feat: replace API link

--- migration-to-typescript/src/components/controller/appLoader.ts
+++ migration-to-typescript/src/components/controller/appLoader.ts
@@ -1,11 +1,12 @@
 import Loader from './loader';
 
 class AppLoader extends Loader {
-    constructor() {
-        super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
-        });
-    }
+  constructor() {
+    super('https://nodenews.herokuapp.com/', {
+      // super('https://newsapi.org/v2/', {
+      apiKey: 'b7d42f96f97143d59d75e512a47a3489',
+    });
+  }
 }
 
 export default AppLoader;
