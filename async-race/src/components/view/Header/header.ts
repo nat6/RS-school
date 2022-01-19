89ae8edae93c569ede007e1a6f371446cc345911
nat6 <nat6dv@gmail.com>
2022-01-19 17:57:05 +0300

    feat: add header

---
+++ async-race/src/components/view/Header/header.ts
@@ -0,0 +1,33 @@
+import Component from '../../../templates/component';
+
+class Header extends Component {
+  public constructor(tagName: string, className: string) {
+    super(tagName, className);
+  }
+
+  protected createHTML(): void {
+    const headerHTML = `
+      <div class="container header__container">
+        <nav class="nav">
+          <ul class="nav__list">
+            <li>
+              <a href="#garage" class="button button_nav" id="garage"> to garage </a>
+            </li>
+            <li>
+              <a href="#winners" class="button button_nav" id="winners"> to winners </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    `;
+
+    this.container.innerHTML = headerHTML;
+  }
+
+  public render(): HTMLElement {
+    this.createHTML();
+    return this.container;
+  }
+}
+
+export default Header;
